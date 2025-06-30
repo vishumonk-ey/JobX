2120be0e102393e9aacbc36535de04b9c01bba05
Vishal <vkant2005@gmail.com>
2025-07-01 01:07:31 +0530

    Add .env to .gitignore and create appwrite services and config

---
+++ src/assets/config.js
@@ -0,0 +1,5 @@
+export const config = {
+    appwriteProjectId : import.meta.env.VITE_PROJECT_ID ,
+    appwriteCollectionId : import.meta.env.VITE_COLLECTION_ID ,
+    appwriteDatabaseId : import.meta.env.VITE_DATABASE_ID
+}
