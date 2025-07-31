478caf8e6d13666e38ffca7ab093012493398a72
Vishal <vkant2005@gmail.com>
2025-07-31 23:47:42 +0530

    enhanced PostForm component with delete functionality and improved error handling

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Input, Button } from "../components/index";
 import { Circle } from "lucide-react";
-import {databaseService} from '../appwrite/databaseService'
+import { databaseService } from "../appwrite/databaseService";
 import { useNavigate } from "react-router-dom";
 function Postform({ JobData }) {
   const {
@@ -20,35 +20,45 @@ function Postform({ JobData }) {
     AppliedBy: JobData?.AppliedBy || "",
     Notes: JobData?.Notes || "",
   });
-  const [error , setError] = useState()
-  const navigate = useNavigate()
-  const onSubmithandler = async(data) => {
+  const [error, setError] = useState();
+  const navigate = useNavigate();
+  const onSubmithandler = async (data) => {
     try {
-      setError("")
-      if(JobData){
-          const isUpdated = await databaseService.updateDocument( 
-            data,
-            JobData.$id
-          )
-          if(isUpdated){
-            navigate(`/view-page/${JobData.$id}`)
-          }
-      }else{
-        const isCreated = await databaseService.createDocument(data)
-        if(isCreated){
-          navigate(`/view-page/${isCreated.$id}`)
+      setError("");
+      if (JobData) {
+        const isUpdated = await databaseService.updateDocument(
+          data,
+          JobData.$id
+        );
+        if (isUpdated) {
+          navigate(`/view-page/${JobData.$id}`);
+        }
+      } else {
+        const isCreated = await databaseService.createDocument(data);
+        if (isCreated) {
+          navigate(`/view-page/${isCreated.$id}`);
         }
       }
     } catch (error) {
       console.log(error);
-      setError(error)
+      setError(error);
     }
-    
   };
+  const deleteJobData = async () => {
+    try {
+      const isDeleted = await databaseService.deleteDocument(JobData.$id);
+      if (isDeleted) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log("error in deleting : ", error);
+    }
+  };
+  const [isModalOpen, setisModalOpen] = useState(false);
   const selectOptions = ["Applied", "Interview", "Rejected", "Offer"];
   return (
-    <div className="w-full mx-auto">
-      <div className="w-full max-w-[700px] p-2">
+    <div className="w-full mx-auto relative ">
+      <div className={`w-full max-w-[700px] p-2 `+ isModalOpen ? "blur-lg overflow-hidden pointer-events-none " : "" }>
         <h1 className="text-4xl">
           {JobData ? "Edit Application" : " New Application"}
         </h1>
@@ -57,7 +67,7 @@ function Postform({ JobData }) {
             Add a new job application to track !
           </p>
         )}
-        <form className="mt-4 space-y-2" onSubmit={onSubmithandler()}>
+        <form className="mt-4 space-y-2" onSubmit={handleSubmit(onSubmithandler())}>
           <div className="w-full flex flex-wrap items-center gap-3">
             <div className="flex-1 min-w-[200px]">
               <Input
@@ -167,14 +177,42 @@ function Postform({ JobData }) {
               {...register("Notes")}
             ></Input>
           </div>
-          {error && (<p className="text-red-400 text-sm">{error}</p>)}
+          {error && <p className="text-red-400 text-sm">{error}</p>}
           <hr className="px-2" />
           <div className="w-full flex flex-col md:flex-row items-center justify-around">
-            <Button onClick={handleSubmit(onSubmithandler())}>{JobData ? "Edit Document" : "Add document"}</Button>
-            {JobData && <Button>Delete document</Button>}
+            <Button type="submit">
+              {JobData ? "Edit Document" : "Add document"}
+            </Button>
+            {JobData && (
+              <Button type="button" onClick={() => setisModalOpen(true)}>
+                Delete document
+              </Button>
+            )}
           </div>
         </form>
       </div>
+      {isModalOpen && (
+        <div className="w-full max-w-xl rounded-lg md:max-w-3xl p-2 md:p-3.5 absolute top-5 left-[50%] transform translate-x-1/2 z-50">
+          <p className="text-lg md:text-xl font-semibold">Delete Job Listing</p>
+          <p className="mt-2 text-gray-400 text-sm">
+            Are you sure that you want to delete the data ? Once the data is
+            lost , you wont be able to recover it !
+          </p>
+          <div className="w-full flex items-center gap-2">
+            <Button className="flex-1" onClick={deleteJobData}>
+              Delete
+            </Button>
+            <Button
+              className="flex-1"
+              onClick={() => {
+                setisModalOpen(!isModalOpen);
+              }}
+            >
+              Cancel
+            </Button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
